14f1415905718b8a8bd5bb9f0faf6c0658908d0b
Evan Gao <notch1p2004@outlook.com>
2024-06-19 01:29:27 +0800

    enable block compilation on sbcl

--- init.lisp
+++ init.lisp
@@ -1,3 +1,4 @@
+#+sbcl (setq *block-compile-default* t)
 (let ((sys (merge-pathnames "cl-welearn.asd" (uiop:getcwd))))
-    (asdf:load-asd sys)
-    (ql:quickload :cl-welearn))
+  (asdf:load-asd sys)
+  (ql:quickload :cl-welearn))
